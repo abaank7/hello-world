621e5c3b7f17f220dc25785bd9a1c27ed907229b
Abaan koul <122855774+abaank7@users.noreply.github.com>
2023-10-23 12:36:56 +0530

    Add files via upload

---
+++ loops ( sum of 100 whole even and odd numbers).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i,sum=0;
+	printf("the sum of whole numbers upto 100\n");
+	for(i=0;i<=100;i++)
+	{
+		sum=sum+i;
+		
+}
+printf("%d\t",sum);
+printf("\n");
+sum=0;
+printf("the sum of natural numbers upto 100\n");
+for(i=1;i<=100;i++)
+{
+	sum=sum+i;
+}
+printf("%d\t",sum);
+return 0;
+}
---
+++ loops ( upper case and lowercase alphabets ).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	char c;
+	printf("uppercase letters from A-Z\n");
+	for(c='A';c<='Z';c++)
+	{
+		printf("%c\t",c);
+	}
+	
+	printf("\n");
+	
+	printf("lowercase letters from a-z\n");
+	
+	for(c='a';c<='z';c++)
+	{
+		printf("%c\t",c);
+	
+	
+	}
+	
+		return 0;
+}
